7db39b2b990830ba8788d5b5c38cc96d69a41e95
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2023-06-19 00:57:20 +0300

    Added documentation

--- CaptainVoiceDiversity_v2/dllmain.cpp
+++ CaptainVoiceDiversity_v2/dllmain.cpp
@@ -13,6 +13,8 @@ void Initialize()
 	//  - Change materials
 }
 
+/// The original main function of the mod. Placed here as context of what came before.
+/*
 member_detour(captainvoicediversity, CaptainVoiceDiversity, void(void*, char8_t*, char8_t*, int32_t)) {
 	void detoured(void* p1, char8_t* p2, char8_t* p3, int32_t p4) {
 		
@@ -20,10 +22,27 @@ member_detour(captainvoicediversity, CaptainVoiceDiversity, void(void*, char8_t*
 		original_function(this,p1, p2, p3, p4);
 	}
 };
+*/
+
+///
+/// Function parameters
+/// int id:  ID of the property function reads from
+/// int* target_data: pointer to the value being written to.
+///
 member_detour(cvd_assigndata, CaptainVoiceDiversity, void(int, int*)) {
 	void detoured(int id, int* target_data) {
+		// We will first allow the original function to run.
 		original_function(this, id, target_data);
+
+		// Then check if ID matches and if target integer is above 5.
 		if (id == 0x39f9e08 && *target_data > 5) {
+			/// If target_data is above 5, change it to 5. The value represents creature type.
+			/// 
+			/// 4 and below is creature, 5 is tribal, and 6 and above are civilian, colonist, etc.
+			/// function at address 0xa3b200 (Disc) / 0xa3b350 (March 2017) checks if target_data is 5. If above 6, it will always run the same voice for every creature.
+			/// 
+			/// In this case, we do not want to have target_data above 5, so we change it to 5 if it goes over.
+			///
 			*target_data = 5;
 		}
 	}
@@ -36,7 +55,7 @@ void Dispose()
 
 void AttachDetours()
 {
-	captainvoicediversity::attach(Address(ModAPI::ChooseAddress(0xa3b200, 0xa3b350)));
+//	captainvoicediversity::attach(Address(ModAPI::ChooseAddress(0xa3b200, 0xa3b350)));
 	cvd_assigndata::attach(Address(ModAPI::ChooseAddress(0xa0fa40,0xa0fa30)));
 
 	// Call the attach() method on any detours you want to add
